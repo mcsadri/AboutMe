536b5b049e057652aacb7eb2275e73860382f4bf
Manuch Sadri <colonquebang@hotmail.com>
2022-07-14 22:32:27 -0700

    completed Lab 02 with updated index.html and style.css

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ alert('Hi, ' + userName + '! Welcome to my page. Ready for some questions?');
 let q1 = 'Am I over 40 years old?';
 let a1 = getInput(q1);
 if (a1 === 'y' || a1 === 'yes'){
-  console.log('That\'s right! I\'m 48 years old.');
+  //console.log('That\'s right! I\'m 48 years old.');
   alert('That\'s right! I\'m 48 years old.');
 } else {
   console.log('Aren\'t you sweet.');
@@ -20,10 +20,10 @@ if (a1 === 'y' || a1 === 'yes'){
 let q2 = 'Do I drive a Honda?';
 let a2 = getInput(q2);
 if (a2 === 'y' || a2 === 'yes'){
-  console.log('Nope, I drive a Saab.');
+  //console.log('Nope, I drive a Saab.');
   alert('Nope, I drive a Saab.');
 } else {
-  console.log('Black Saab rules!');
+  //console.log('Black Saab rules!');
   alert('Black Saab rules!');
 }
 
@@ -31,39 +31,39 @@ if (a2 === 'y' || a2 === 'yes'){
 let q3 = 'The Seattle Sounders are the greatest team you\'ve ever seen.';
 let a3 = getInput(q3);
 if (a3 === 'y' || a3 === 'yes'){
-  console.log('Eternal blue, forver green!');
+  //console.log('Eternal blue, forver green!');
   alert('Eternal blue, forver green!');
 } else {
-  console.log('You\'re wrong, and you should feel bad.');
+  //console.log('You\'re wrong, and you should feel bad.');
   alert('You\'re wrong, and you should feel bad.');
 }
 
-// question 4: How do I like my Scotch?
-let q4 = 'Do I like my Scotch whisky unpeated?';
+// question 4: ice cream?
+let q4 = 'Do you like ice cream?';
 let a4 = getInput(q4);
 if (a4 === 'y' || a4 === 'yes'){
-  console.log('Nah, that\'s boring. Give me the smoke.');
-  alert('Nah, that\'s boring. Give me the smoke.');
-} else {
-  console.log('I wasn\'t always so enlighted, but I got better.');
-  alert('I wasn\'t always so enlighted, but I got better.');
-}
-
-// question 5: ice cream?
-let q5 = 'Do you like ice cream?';
-let a5 = getInput(q5);
-if (a5 === 'y' || a5 === 'yes'){
-  console.log('Me too. I like Mint Chocolate Chip.');
+  //console.log('Me too. I like Mint Chocolate Chip.');
   alert('Me too. I like Mint Chocolate Chip.');
 } else {
-  console.log('Bummer. I feel sad for you.');
+  //console.log('Bummer. I feel sad for you.');
   alert('Bummer. I feel sad for you.');
+}
+
+// question 5: Too many pop-ups?
+let q5 = 'This really is death by a thousand pop-ups?';
+let a5 = getInput(q5);
+if (a5 === 'y' || a5 === 'yes'){
+  //console.log('I agree, can we done now please?');
+  alert('I agree, ' + userName + ', can we be done now please?');
+} else {
+  //console.log('You\'re a pop-up masochist. You can\'t be trusted.');
+  alert('You\'re a pop-up masochist, ' + userName + '. You can\'t be trusted.');
 }
 
 // get user input and validate/normalize it
 function getInput(question){
   let userInput = prompt(question).toLowerCase();
-  console.log(userInput);
+  //console.log(userInput);
   if (userInput === 'y' || userInput === 'yes' || userInput === 'n' || userInput === 'no' ){
     return userInput;
   } else {
@@ -73,6 +73,7 @@ function getInput(question){
 }
 
 // display user's name on webpage greeting
+// eslint-disable-next-line
 function getGreeting(){
   document.write('<p>Hi, ' + userName + '!</p>');
 }
