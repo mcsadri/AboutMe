51c1920ebdeca488df55b09714e9c9cc60d7662d
Manuch Sadri <colonquebang@hotmail.com>
2022-07-18 20:19:17 -0700

    corrected app.js line 15 to correct the TypeError: Cannot read properties of undefined (reading '0') error. And removed else if from initial for loop to remove faulty logic & user prompt feedback

--- js/app.js
+++ js/app.js
@@ -1,36 +1,40 @@
 'use strict';
 
+// get user's name & provide greeting
+let userName = prompt('What\'s your first name?');
+//console.log(userName);
+document.write('<h2>Hi, ' + userName + '!</h2>');
+
+// variable to count user correct guesses
 let count = 0;
 
 // refactor the original Code 201 Lab 02 AboutMe assignment's 5 yes/no questions using loops and arrays
 //declare multidimensional array where each nested array has three string values. Array questions will be used in the following for loop
 let questions = [
-    ['Am I over 40 years old?', 'y', 'yes'],
-    ['Do I drive a Saab?', 'y', 'yes'],
-    ['The Seattle Sounders are the greatest team you\'ve ever seen.', 'y', 'yes'],
-    ['Do you like ice cream?', 'y', 'yes'],
-    ['This really is death by a thousand pop-ups?', 'y', 'yes']
+    ['Am I over 40 years old?', 'y'],
+    ['Do I drive a Saab?', 'y'],
+    ['The Seattle Sounders are the greatest team you\'ve ever seen.', 'y'],
+    ['Do you like ice cream?', 'y'],
+    ['This really is death by a thousand pop-ups?', 'y']
 ];
 // loop to ask and evaluate each nested array question
-for (let i = 0; i < (questions.length + 1); i++){
+for (let i = 0; i < questions.length; i++){
     let answer = '';
-    console.log(questions[i][0]);
+    //console.log(questions[i][0]);
     // while answer is not a valid response (y or n) prompt user with question and collect response
-    while (answer !== 'y' && answer !== 'yes' && answer !== 'n' && answer !== 'no'){
+    while (answer !== 'y' && answer !== 'n'){
         // get user input and convert to lower case
         answer = prompt(questions[i][0]).toLowerCase();
-        console.log(answer);
-        // if answer equals values in index 1 and 2 in array then increment count
-        if (answer === questions[i][1] || answer === questions[i][2]){
+        //console.log('answer ' + i + ' = ' + answer);
+        answer = answer.substring(0, 1);
+        //console.log('answer substring' + i + ' = ' + answer);
+        // if answer equals values in index [i][1] then increment count
+        if (answer === questions[i][1]){
             count++;
-            console.log(count);
-        } // else if prompt user to resubmit their reponse
-        else if (answer !== 'y' || answer !== 'yes' || answer !== 'n' || answer !== 'no'){
-            alert('Please answer with Y, Yes, N, or No.');
+            //console.log('count = ' +count);
         }
     }
 }
-alert('You answered ' + count + ' questions correctly.');
 
 
 // Code 201 Lab 03 user story #2 (aka question #6, give the user 4 chances to guess a random number between 1-10)
@@ -74,7 +78,7 @@ for (let i = 0; i < states.length; i++){
 }
 // give the user 6 changes to guess a correct answer
 for (let i = 0; i < 6; i++){
-    console.log('i = ' + i);
+    //console.log('i = ' + i);
     // prompt user for guess and convert to upper case
     let userGuess2 = prompt('Can you guess one of the US states I\'ve lived in?\nUse 2-character abbreviatons only').toUpperCase();
     //console.log('userGuess2 = ' + userGuess2);
@@ -100,4 +104,4 @@ for (let i = 0; i < 6; i++){
 
 
 // Code 201 Lab 03 user story #4 (provide user with their guessing game score)
-alert('You guessed ' + count + ' of 7 questions correctly.');
+alert( userName + ', you guessed ' + count + ' of 7 questions correctly.');
--- js/app1.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// get user's name & provide greeting
-let userName = prompt('What\'s your first name?');
-//console.log(userName);
-document.write('<h2>Hi, ' + userName + '!</h2>');
