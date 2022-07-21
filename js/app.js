f5fd1690c6c3c7038a38279aed3c104ddc6f9ed4
Manuch Sadri <colonquebang@hotmail.com>
2022-07-20 21:14:20 -0700

    moved js/app.js function calls to top of file, and debugged score counter

--- js/app.js
+++ js/app.js
@@ -1,19 +1,29 @@
 'use strict';
 
-function getName(){
-// get user's name & provide greeting
-    let userName = prompt('What\'s your first name?');
-    //console.log(userName);
-    document.write('<h2>Hi, ' + userName + '!</h2>');
-}
-getName();
-
-// global variable to count user's correct guesses
+// global variable to keep score of correct quesses
 let count = 0;
 
+// function calls to run guessing game and provide score results
+let userName = getName();
+count = fiveQuestions(count);
+//console.log('count.fiveQuestions = ' + count);
+count = guessNumber(count);
+//console.log('count.guessNumbers = ' + count);
+count = guessState(count);
+//console.log('count.fiveState = ' + count);
+returnScore(userName, count);
+
+// get user's name & provide greeting
+function getName(userName){
+    userName = prompt('What\'s your first name?');
+    //console.log(userName);
+    document.write('<h2>Hi, ' + userName + '!</h2>');
+    return userName;
+}
+
 // refactor the original Code 201 Lab 02 AboutMe assignment's 5 yes/no questions using loops and arrays
-// declare multidimensional array where each nested array has two string values: [question, answer]
 function fiveQuestions(count) {
+    // declare multidimensional array where each nested array has two string values: [question, answer]
     let questions = [
         ['Am I over 40 years old?', 'y'],
         ['Do I drive a Saab?', 'y'],
@@ -35,18 +45,17 @@ function fiveQuestions(count) {
             //console.log('answer substring' + i + ' = ' + answer);
             // if answer equals values in index [i][1] then increment count
             if (answer === questions[i][1]){
-                // count++;
-            //console.log('count = ' +count);
+                count++;
+                //console.log('count = ' +count);
             }
         }
     }
     return count;
 }
-fiveQuestions(count);
 
 // Code 201 Lab 03 user story #2 (aka question #6, give the user 4 chances to guess a random number between 1-10)
-// set a random whole number (1-10) value (taken from www.w3schools.com/js/js_random.asp)
 function guessNumber(count){
+    // set a random whole number (1-10) value (taken from www.w3schools.com/js/js_random.asp)
     let target = Math.floor(Math.random() * 11);
     //console.log('target = ' + target);
     // give the user 4 guesses pick the right number
@@ -59,9 +68,10 @@ function guessNumber(count){
     if (userGuess1 == target){
             alert('Wow, great guess!');
             count++; // if correct guess increment correct guesses count
-            break;
+            return count;
         } else if (i === 3) {
             alert('You\'re out of guesses. The correct answer was ' + target + '.');
+            return count;
         } else if (userGuess1 < target) {
             alert('You\'ve guessed too low. You have ' + (3 - i) + ' guesses remaining.');
         } else if (userGuess1 > target) {
@@ -70,13 +80,11 @@ function guessNumber(count){
             alert('Please enter a valid number. You have ' + (3 - i) + ' guesses remaining.');
         }
     }
-    return count;
 }
-guessNumber(count);
 
 // Code 201 Lab 03 user story #3 (aka question #7, guessing game using an array to hold multiple possible correct answers)
-// decalre an array of US state abbreviations as possible correct answers for the user to guess
 function guessState(count) {
+    // decalre an array of US state abbreviations as possible correct answers for the user to guess
     let states = ['CO', 'KS', 'MO', 'WA'];
     // read states array elements into a single string for use later in user alerts
     let stateList = '';
@@ -103,15 +111,17 @@ function guessState(count) {
         if (found === true){
             alert('Wow, great guess! \nThe full list of correct answers is: ' + stateList + '.');
             count++; // increment correct guesses count
-            break;
+            return count;
         } else if (i === 5){
             alert('You\'re out of guesses. \nThe list of correct answers is: ' + stateList + '.');
+            return count;
         } else {
             alert('That\'s incorrect. You have ' + (5 - i) + ' guesses remaining.');
         }
     }
-    return count;
 }
-guessState(count);
+
 // Code 201 Lab 03 user story #4 (provide user with their guessing game score)
-alert('you guessed ' + count + ' of 7 questions correctly.');
+function returnScore(userName, count){
+    alert(userName + ', you guessed ' + count + ' of 7 questions correctly.');
+}
